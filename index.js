65f73ccc452de498311e303955f0888643e17f58
jcppman <jcppman@gmail.com>
2014-05-21 17:30:59 +0800

    Tests added

--- index.js
+++ index.js
@@ -1,45 +1,55 @@
-module.exports = function (inRules) {
-
-  var rules = inRules;
-
-  return (function (inStr) {
+function ParamsParser(rules) {
   
-    var params;
+  if (!rules || !rules.length) {
 
-    for (var i=0; i<rules.length; i++) {
+    throw new Error('INVALID_RULES * err -> INVALID_RULES * rules -> ' + rules);
+  
+  }
 
-      var rule = rules[i];
-      var reg = rule.reg;
-      var getter = rules.getter;
-      var result;
+  this.rules = rules;
 
-      if (typeof getter !== 'function') {
+};
 
-        throw new Error('INVALID_RULE * err -> getter should be a function');
-      
-      }
+ParamsParser.prototype.parse = function (inStr) {
+  
+  var that = this;
+  var rules = that.rules;
+  var params;
 
-      if (reg) {
+  for (var i=0; i<rules.length; i++) {
 
-        var regResult = inStr.match(reg);
-        result = getter(regResult);
-      
-      } else {
+    var rule = rules[i];
+    var reg = rule.reg;
+    var getter = rule.getter;
+    var result;
 
-        result = getter(inStr);
-      
-      }
+    if (typeof getter !== 'function') {
 
-      if (result) {
-      
-        return result;
+      throw new Error('PARAMS_PARSER * err -> INVALID_RULE: getter should be a function');
+    
+    }
 
-      }
+    if (reg) {
+
+      var regResult = inStr.match(reg);
+      result = getter(regResult);
+    
+    } else {
+
+      result = getter(inStr);
+    
+    }
+
+    if (result) {
+    
+      return result;
 
     }
 
-    throw new Error('PARAMS_PARSER * err -> NO_MATCH_RULE * input -> ' + inStr);
+  }
 
-  });
+  throw new Error('PARAMS_PARSER * err -> NO_MATCH_RULE * input -> ' + inStr);
 
 };
+
+module.exports = ParamsParser;
